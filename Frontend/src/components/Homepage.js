9fb84de1f9a2c30876814f569644a777d303d79d
JonVain <jonne.vainikainen@edu.savonia.fi>
2023-02-10 14:50:13 +0200

    #22 MUI lisätty

--- Frontend/src/components/Homepage.js
+++ Frontend/src/components/Homepage.js
@@ -1,5 +1,8 @@
+import Button from '@mui/material/Button';
+
 export default function Homepage() {
     return <div>
         <h1>Etusivu</h1>
+        <Button variant="contained">Hello World</Button>
     </div>
 }
