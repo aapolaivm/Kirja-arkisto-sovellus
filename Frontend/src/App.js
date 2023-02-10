75051ce1deecd7132145c6a4070c78ec2f200b2b
JonVain <jonne.vainikainen@edu.savonia.fi>
2023-02-10 17:06:08 +0200

    #22 Navigointi etusivulla

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,10 +7,12 @@ import '@fontsource/roboto/700.css';
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
 import Homepage from "components/Homepage";
+import Demo from "components/Demo";
 
 const App = () => {
     return (
         <div>
+            <Demo></Demo>
             <Routes>
                 <Route exact path="/" element={<Homepage />} />
             </Routes>
