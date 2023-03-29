daac08d9dece044d077a5310843afa5f6edbc010
JonVain <jonne.vainikainen@edu.savonia.fi>
2023-03-29 12:40:45 +0300

    Arkisto suunnittelu

--- Frontend/src/pages/Arkisto.js
+++ Frontend/src/pages/Arkisto.js
@@ -1,12 +1,26 @@
 import React from "react";
-import NavBar from "components/NavBar";
+import ArkistoTaulukko from "components/ArkistoTaulukko"
+import SearchBar from "components/SearchBar"
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 
 const Arkisto = () => {
     return (
-        <div>
-            <NavBar/>
+        <>            
             <h1>Arkisto</h1>
-        </div>
+            <SearchBar></SearchBar>
+            <br></br>
+            <Stack spacing={2} direction="row">      
+      <Button variant="contained">Etsi</Button>
+      <Button variant="contained">Tyhjennä</Button>
+      
+    </Stack>
+            <br></br>
+            
+            <ArkistoTaulukko></ArkistoTaulukko>       
+        </>
+        
     );
 };
 
